77d808f9b39033be97d24c954a4942854b20e973
Amanda <amandasanzlop@hotmail.com>
2020-01-25 18:42:39 +0000

    Arreglar casilla

--- HundirLaFlota/src/hundirlaflota/Casilla.java
+++ HundirLaFlota/src/hundirlaflota/Casilla.java
@@ -3,30 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package HundirLaFlota;
+package hundirlaflota;
 
 /**
  *
  * @author Amanda
  */
 public class Casilla {
+    private int x;
+    private int y;
+    private Barco barco;            //no entiendo para q (duda de alanis)
+    private boolean fueBombardeada;
+    private String value;
 
-    int x;
-    int y;
-    Barco barco;
-    boolean fueBombardeada;
+    //-----------------------------------------------------------------------------
 
+    public Casilla(){}
     /**
      * Constructor para crear casilla sin barco, sólo agua.
      * @param x
      * @param y 
      */
     public Casilla(int x, int y) {
-
         this.x = x;
         this.y = y;
-        this.fueBombardeada = false;
-        
+        this.fueBombardeada = false;       
     }
     
     /**
@@ -35,21 +36,62 @@ public class Casilla {
      * @param y
      * @param barco 
      */
-    public Casilla(int x, int y, Barco barco){
-        
+    public Casilla(int x, int y, Barco barco){        
         this.x = x;
         this.y = y;
         this.fueBombardeada = false;
         this.barco = barco;
     }
     
-    public boolean obtenerFueBombardeada(){
-        
+    //-----------------------------------------------------------------------------
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Barco getBarco() {
+        return barco;
+    }
+
+    public void setBarco(Barco barco) {
+        this.barco = barco;
+    }
+
+    public boolean isFueBombardeada() {
+        return fueBombardeada;
+    }
+
+    public void setFueBombardeada(boolean fueBombardeada) {
+        this.fueBombardeada = fueBombardeada;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    //-----------------------------------------------------------------------------
+   
+    public boolean obtenerFueBombardeada(){    
         return this.fueBombardeada;
     }
     
-    public boolean contieneBarco(){
-        
+    public boolean contieneBarco(){       
         if (this.barco != null){
             return true;
         }else{
@@ -57,17 +99,15 @@ public class Casilla {
         }
     }
     
-    public void recibirBombardeo() throws Exception{
-        
+    public void recibirBombardeo() throws Exception{     
         if (obtenerFueBombardeada()){
             throw new Exception ("Ya fue bombardeada");
-        }
-        
-        this.fueBombardeada = true;
-        
-        if (contieneBarco()){
-            
+        }      
+        this.fueBombardeada = true;        
+        if (contieneBarco()){           
             this.barco.recibirImpacto();
         }
     }
+
+   
 }
