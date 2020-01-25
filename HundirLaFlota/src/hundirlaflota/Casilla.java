951ba56d3ba5d8dad887a793db0581d59886c643
Amandasanzlop <60135295+Amandasanzlop@users.noreply.github.com>
2020-01-25 16:48:42 +0000

    Add files via upload
    Clase Casilla

---
+++ HundirLaFlota/src/hundirlaflota/Casilla.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HundirLaFlota;
+
+/**
+ *
+ * @author Amanda
+ */
+public class Casilla {
+
+    int x;
+    int y;
+    Barco barco;
+    boolean fueBombardeada;
+
+    /**
+     * Constructor para crear casilla sin barco, sólo agua.
+     * @param x
+     * @param y 
+     */
+    public Casilla(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+        this.fueBombardeada = false;
+        
+    }
+    
+    /**
+     * Constructor para crear casilla ocupada por barco.
+     * @param x
+     * @param y
+     * @param barco 
+     */
+    public Casilla(int x, int y, Barco barco){
+        
+        this.x = x;
+        this.y = y;
+        this.fueBombardeada = false;
+        this.barco = barco;
+    }
+    
+    public boolean obtenerFueBombardeada(){
+        
+        return this.fueBombardeada;
+    }
+    
+    public boolean contieneBarco(){
+        
+        if (this.barco != null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void recibirBombardeo() throws Exception{
+        
+        if (obtenerFueBombardeada()){
+            throw new Exception ("Ya fue bombardeada");
+        }
+        
+        this.fueBombardeada = true;
+        
+        if (contieneBarco()){
+            
+            this.barco.recibirImpacto();
+        }
+    }
+}
